822b9907859b2b02ef6102305239bdbf075c60cb
Tad Keller <logisch@pm.me>
2022-03-31 22:33:22 -0500

    update ui

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from ttkthemes import ThemedStyle
 
 import os
 from os.path import exists
@@ -9,22 +10,21 @@ import time
 from random import random
 from datetime import date
 
+
 #so the save function knows in which order to stitch videos in
 firstFlag = True
-
-
 
 #record two 10 minute videos back to back for stitching
 def handleRecord():
     while True:
         
         firstFlag = True
-        os.system("./libcamera-vid -t 600000 -o /media/pi/videos/unsaved/primary.h264 -s")
+        os.system("./libcamera-vid --nopreview -t 600000 -o /media/pi/videos/unsaved/primary.h264 -s")
         if exitFlag:
             break
         
         firstFlag = False
-        os.system("./libcamera-vid -t 600000 -o /media/pi/videos/unsaved/secondary.h264 -s")
+        os.system("./libcamera-vid --nopreview -t 600000 -o /media/pi/videos/unsaved/secondary.h264 -s")
         if exitFlag:
             break
         
@@ -65,6 +65,9 @@ thread1 = threading.Thread(target=handleRecord)
 thread1.start()
 
 window = Tk()
+window.geometry("800x480")
+style = ThemedStyle(window)
+style.set_theme("equilux")
 
 def Shutdown():
     with open("test.txt") as f:
@@ -79,14 +82,21 @@ def Shutdown():
     window.destroy()
 
 # setting attribute
-window.attributes('-fullscreen', True)
-frm = ttk.Frame(window, padding=10)
-frm.grid()
+#window.attributes('-fullscreen', True)
+leftFrame = ttk.Frame(window, padding=0)
+leftFrame.pack(side=LEFT, expand=True, fill='both')
+rightFrame = ttk.Frame(window, padding=0)
+rightFrame.pack(side=RIGHT, expand=True, fill='both')
 
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Shutdown", command=Shutdown).grid(column=1, row=0)
-ttk.Button(frm, text="Stop Recording", command=stopVideo).grid(column=2, row=0)
-ttk.Button(frm, text="Save Recording", command=saveVideo).grid(column=3, row=0)
+ttk.Button(leftFrame, text="Shutdown", command=Shutdown).grid(column=0, row=3)
+ttk.Button(leftFrame, text="Stop Recording", command=stopVideo).grid(column=0, row=2)
+ttk.Button(leftFrame, text="Save Recording", command=saveVideo).grid(column=0, row=0)
+ttk.Button(leftFrame, text="Preview Camera").grid(column=0, row=1)
+currentSpeed = Label(rightFrame, height=1, width=3, bg="black", fg="gray", text="55", font=("Arial, 150"))
+currentSpeed.grid(column=3, row=0)
+readSpeed = Label(rightFrame, height=1, width=3, bg="black", fg="gray", text="50", font=("Arial, 150"))
+readSpeed.grid(column=3, row=1)
+
 
 window.mainloop()
 
